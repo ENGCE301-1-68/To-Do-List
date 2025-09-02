7c81d43f82e5976e6cf7e25c34830c2e1742bb81
Sittinon <sittinon_yo66@live.rmutl.ac.th>
2025-09-02 18:17:13 +0700

    initial commit for React To-Do List project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TodoAppContainer from './containers/TodoAppContainer';
+import './App.css';
+
+function App() {
+  // 🔽 ไม่มี state ไม่มี logic มีแค่การเรียกใช้ Container
+  return <TodoAppContainer />;
+}
+
+export default App;
